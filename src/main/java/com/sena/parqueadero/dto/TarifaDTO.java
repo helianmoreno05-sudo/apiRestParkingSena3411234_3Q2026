37e366bc639e28d6359790c80656eefbcf07b3c1
HCML <helianmoreno35@gmail.com>
2026-07-16 23:23:40 -0500

    v0.4_clasesPaqueteDTO_HCML_16072026

--- src/main/java/com/sena/parqueadero/dto/TarifaDTO.java
+++ src/main/java/com/sena/parqueadero/dto/TarifaDTO.java
@@ -1,5 +1,24 @@
 package com.sena.parqueadero.dto;
 
+import com.sena.parqueadero.model.TipoVehiculo;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+
 public class TarifaDTO {
+
+	private Long idTarifa;
+	@NotNull(message = "El tipo de vehiculo es obligatorio")
+
+	private TipoVehiculo tipoVehiculo;
+	@NotNull(message = "La tarifa por hora es obligatoria")
+	@Positive(message = "La tarifa por hora debe ser mayor a cero")
+	private Double tarifaPorHora;
+	@NotNull(message = "La tarifa por dia es obligatoria")
+	@Positive(message = "La tarifa por dia debe ser mayor a cero")
+	private Double tarifaPorDia;
 
 }
