acb58483e9e69f7704a64cb6d95e68a1886a5906
HCML <helianmoreno35@gmail.com>
2026-07-16 19:44:19 -0500

    v0.2_configuracionAplication.properties_conexionDB_HCML_16072026

--- src/main/java/com/sena/parqueadero/model/Registro.java
+++ src/main/java/com/sena/parqueadero/model/Registro.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,6 +25,8 @@ import lombok.ToString;
 @Table(name = "Registros")
 
 public class Registro {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idRegistro;
 	@Column(nullable = false)
 	private LocalDateTime FechaHoraIngreso;
